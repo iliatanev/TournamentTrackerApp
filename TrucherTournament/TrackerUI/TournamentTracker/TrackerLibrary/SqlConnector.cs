afeaeb9249a2785bc4544e787b5aa7b90bab88aa
itanev <iliatanew@gmail.com>
2022-09-02 17:10:22 +0300

    interface and sql connector

---
+++ TrucherTournament/TrackerUI/TournamentTracker/TrackerLibrary/SqlConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    internal class SqlConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
